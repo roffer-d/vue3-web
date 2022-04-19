8638c17bfec8b32b8802814e6c604ad6ba3dc047
497664492@qq.com <497664492@qq.com>
2022-04-19 16:52:26 +0800

    增加注释

--- src/view/login/api.js
+++ src/view/login/api.js
@@ -4,10 +4,26 @@ const api = {
     logOut:'/logOut',//退出登录
 }
 
+/**
+  * @desc 登录
+  * @params:
+  *   params(Object): 参数
+  * @auth Dulongfei
+  * 2022/4/19 16:51
+  *
+  */
 export function login(params={}){
     return form(api.login,params)
 }
 
+/**
+  * @desc 退出登录
+  * @params:
+  *   params(Object): 参数
+  * @auth Dulongfei
+  * 2022/4/19 16:51
+  *
+  */
 export function logOut(params={}){
     return form(api.logOut,params)
 }
